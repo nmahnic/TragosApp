24c0a8d36ae69ffb224e22555d3bb01fafbf0c9d
Nicolas Mahnic <nico.mahnic94@gmail.com>
2021-01-01 19:29:48 -0300

    first part complete

---
+++ app/src/main/java/com/tragosapp/ui/MainFragment.kt
@@ -0,0 +1,33 @@
+package com.tragosapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.tragosapp.R
+
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnIrDetalles.setOnClickListener {
+            findNavController().navigate(R.id.tragosDetalleFragment)
+        }
+    }
+
+}
